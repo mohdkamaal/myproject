4fc60de1010c5c26fc23ac3479d123c9618f180a
mohdkamaal <mohd.kamaal10@gmail.com>
2020-02-28 15:51:51 +0530

    admin

--- src/components/Admin/VATSW.vue
+++ src/components/Admin/VATSW.vue
@@ -3,41 +3,42 @@
     <v-container>
       <h1 class="text-center">View All Teacher Subject Wise</h1>
       <v-divider></v-divider>
-
-      <v-expansion-panels>
-        <v-expansion-panel v-for="subjects in expand" :key="subjects">
-          <v-expansion-panel-header>{{subjects}}</v-expansion-panel-header>
-          <v-expansion-panel-content>
-            <v-row>
-              <v-col cols="4" v-for="(teacher, index) in balance" :key="index">
-                <v-card>
-                  <v-list>
-                    <v-list-item>
-                      <v-list-item-avatar color="grey" size="100"></v-list-item-avatar>
-                      <v-list-item-content>
-                        <v-list-item-title class="title">
-                          {{
-                          teacher.name
-                          }}
-                        </v-list-item-title>
-                        <v-divider></v-divider>
-                        <v-list-item-text class="body-2">
-                          <p>Class : {{ teacher.class }}</p>
-                          <p>
-                            Status :
-                            <v-chip color="green" class="white--text">{{ teacher.status }}</v-chip>
-                          </p>
-                          <p background-color="purple" class>{{ teacher.time }}</p>
-                        </v-list-item-text>
-                      </v-list-item-content>
-                    </v-list-item>
-                  </v-list>
-                </v-card>
-              </v-col>
-            </v-row>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels>
+      <div class="ma-12">
+        <v-expansion-panels>
+          <v-expansion-panel v-for="subjects in expand" :key="subjects">
+            <v-expansion-panel-header>{{subjects}}</v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <v-row>
+                <v-col cols="4" v-for="(teacher, index) in balance" :key="index">
+                  <v-card>
+                    <v-list>
+                      <v-list-item>
+                        <v-list-item-avatar color="grey" size="100"></v-list-item-avatar>
+                        <v-list-item-content>
+                          <v-list-item-title class="title">
+                            {{
+                            teacher.name
+                            }}
+                          </v-list-item-title>
+                          <v-divider></v-divider>
+                          <v-list-item-text class="body-2">
+                            <p>Class : {{ teacher.class }}</p>
+                            <p>
+                              Status :
+                              <v-chip color="green" class="white--text">{{ teacher.status }}</v-chip>
+                            </p>
+                          </v-list-item-text>
+                        </v-list-item-content>
+                      </v-list-item>
+                    </v-list>
+                    <p background-color="red lighten-4" class="text-center">{{ teacher.time }}</p>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </div>
     </v-container>
   </div>
 </template>
